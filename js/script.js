cd90d19042ee57e35604c76b294ef7e87def5d11
Andrey Dukhin <service@goldfish.su>
2024-03-31 07:56:44 +0300

    feat: 15 lesson

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 "use strict";
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+if (numberOfFilms < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms < 31){
+    console.log('Просмотрено довольно мало фильмов');
+} else {
+    console.log('Вы киноман');
+}
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,12 +18,13 @@ const personalMovieDB = {
     privat: false
 };
 
-const m1 = prompt("Один из последних просмотренных фильмов?");
-const r1 = +prompt("На сколько оцените его?");
-const m2 = prompt("Один из последних просмотренных фильмов?");
-const r2 = +prompt("На сколько оцените его?");
+let m1, r1
+for (let i = 1; i <= 2; i++){
+    do {
+        m1 = prompt("Один из последних просмотренных фильмов?");
+    } while (m1 === null || m1.length === 0 || m1.length > 50)
+    r1 = +prompt("На сколько оцените его?");
 
-personalMovieDB.movies[m1] = r1;
-personalMovieDB.movies[m2] = r2;
-
+    personalMovieDB.movies[m1] = r1;
+}
 console.log(personalMovieDB);
---
+++ js/script02.js
@@ -0,0 +1,5 @@
+"use strict";
+
+if (4 === 4) {
+    console.log("Ok!");
+}
